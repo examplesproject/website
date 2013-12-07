ad70a7774a8f10b140455f63c66856717ffee577
examplesproject <info@examplesproject.org.uk>
2013-12-07 16:35:09 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    concurrent: {
+
+      main: ['nodemon', 'watch'],
+      options: {
+        logConcurrentOutput: true
+      }
+    },
+    nodemon: {
+
+      main: {
+
+        file: 'app.js',
+        //watchedFolders: ['modules'],
+        options: {
+
+          ignoredFiles: ['node_modules/**', 'Gruntfile.js']
+        }
+      }
+    },
+    stylus: {
+      compile: {
+        files: {
+          'public/css/main.css': ['stylus/**/*.styl']
+        },
+        options: {
+          compress: false
+        }
+      }
+    },
+    watch: {
+
+      options: {
+
+        interval: 3000
+      },
+      gruntfile: {
+        files: 'Gruntfile.js'
+      },
+      main: {
+        files: ['app.js'],
+        options: {
+          livereload: true
+        }
+      },
+      hbs: {
+        files: ['views/**/*.hbs'],
+        options: {
+          livereload: true
+        }
+      },
+      stylus: {
+        files: ['stylus/**/*.styl'],
+        tasks: 'stylus',
+        options: {
+          atBegin: true
+        }
+      },
+      css: {
+        files: ['public/css/**/*.css'],
+        options: {
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  // Default task.
+  grunt.registerTask('default', ['concurrent:main']);
+};
